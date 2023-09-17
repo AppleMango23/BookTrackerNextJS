6be1383addbf99e9085553fbcb1d6d817d89a9e2
AppleMango23 <ninjayek@gmail.com>
2023-09-17 10:58:21 +0800

    Added texts with underlined and click me button

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1 className="text-center underline">NextJS</h1>
+      <button className="btn">click me</button>
     </main>
   );
 }
